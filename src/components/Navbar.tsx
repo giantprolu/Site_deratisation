de94862c8990c91a320f09cfc2620b8ad621d773
Nathan CHAVAUDRA <nathan.chavaudra@exakis-nelite.com>
2025-02-06 16:03:53 +0100

    logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,7 +9,6 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
           <div className="flex items-center">
-            <img src="src/Asset/logo.webp" alt="Logo" className="h-8 w-8 mr-2" />
             <span className="text-2xl font-bold text-blue-600">Hygiène Protect 3D</span>
           </div>
           
